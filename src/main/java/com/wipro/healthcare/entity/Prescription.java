b29abfbc9e9d5cc57f595e586c27ca4c650bf6d8
Yuvarani J <yu20696578@wipro.com>
2026-06-07 14:10:18 +0530

    Revert "Added JWT security, user auth and prescription module"
    This reverts commit f2fcdb90e4702c96f4442eaa29f32827279c92f0.

--- src/main/java/com/wipro/healthcare/entity/Prescription.java
+++ src/main/java/com/wipro/healthcare/entity/Prescription.java
@@ -1,14 +1,10 @@
 package com.wipro.healthcare.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Data
--- src/main/java/com/wipro/healthcare/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.healthcare.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.wipro.healthcare.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
